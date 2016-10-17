79df3c5f4fc691f719f84e22d9655b218f628c63
Andrew Diakov <berloga.bob@gmail.com>
2016-10-17 16:50:32 +0300

     всё работает

--- testAV2.py
+++ testAV2.py
@@ -5,6 +5,11 @@ import numpy as np
 
 """
 07102016-2.dat
+
+14102016-1.dat
+
+14102016-2.dat
+
 """
 
 usinp = (raw_input('-> '))#ввод имени, открываемого файла
@@ -13,10 +18,29 @@ len_usinp = len(str(usinp))
 #print type(len_usinp)
 usinp_e = usinp[0:-4] + 'e' + usinp[-4:len_usinp]
 #print usinp_e
+#название файла без разширения, для отображения на граффике
+usinp_title = usinp[0:-4]
+#
 s = open(usinp).read()#прочитали исходный файл
 
+#блок замен
+s = s.replace('"', '')#удаляем кавычки
+s = s.replace('sec', '"sec"')#удаляем кавычки
+s = s.replace('mA', '"mA"')#удаляем кавычки
+s = s.replace('mV', '"mV"')#удаляем кавычки
+s = s.replace('    ', '\t')#меняем запятую на точкуу
+s = s.replace(',', '.')#меняем запятую на точкуу
+#s = s.replace('\n', ', ')#замена новой строки на запятую
+#s = s.replace(',', '\t')#замена запятой символом табуляции
+#s = s.replace('.', ',')#замена точки заятой
+
+#создаем новый файл для записывания в него изменений
+f = open(usinp_e, 'w')#открыли файл (создали)
+f.write(s)#записали
+f.close()#закрыли
+
 #создаем датафрэйм
-frame = pd.read_csv(usinp, sep = "\t", header = None, names = ['sec', 'mA', 'mV'], skiprows = 1)
+frame = pd.read_csv(usinp_e, sep = "\t", header = None, names = ['sec', 'mA', 'mV'], skiprows = 1)
 
 allarray = np.array(frame)
 #print allarray
@@ -26,29 +50,23 @@ ma = np.array(frame['mA'])
 #print y
 mv = np.array(frame['mV'])
 #print mv
-
-#plt.figure(1)
-#plt.subplot(211)
+#зона вывода графф
+plt.figure(num = None, figsize=(10,6), dpi= 150)
+plt.suptitle(usinp_title, fontsize=16)
+plt.subplots_adjust(hspace=0.4)#
+#первый графф
+plt.subplot(2,1,1)
 plt.plot(sec, ma)
 plt.xlabel('time, sec')
 plt.ylabel('I, mA')
-plt.title(u'Изменение тока по времени')
+plt.title(u'Изменение тока по времени', fontsize=12)
 plt.grid(True)
-
-
-plt.figure.SubplotParams(left=1, bottom=1, right=1, top=1, wspace=1, hspace=1)
-plt.figure.Figure(figsize={5:3}, dpi=300)
-
-
-"""
-plt.subplot(212)
+#второй графф
+plt.subplot(2,1,2)
 plt.plot(mv, ma, '-r')
 plt.xlabel('Volts, mV')
 plt.ylabel('I, mA')
-plt.title(u'Вольт-амперная характеристика')
+plt.title(u'Вольт-амперная характеристика', fontsize=12)
 plt.grid(True)
-"""
-
-
+#вывод
 plt.show()
-
